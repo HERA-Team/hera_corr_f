4bb8b95c3b2db317a17d05c2983e1c1d0ef692a9
Jack Hickish <jackh@acme1.acme.pvt>
2017-01-21 15:05:11 -0800

    Remove now superfluous blackbox tcl files
    The tcl associated with blackboxes is handled by the IP yellow block

--- pfb_fir_2048ch_core.tcl
+++
@@ -1,4 +0,0 @@
-set repos [get_property ip_repo_paths [current_project]]
-set_property ip_repo_paths "$repos /home/jackh/github/herafengine/pfb_fir_2048ch_core/ip" [current_project]
-update_ip_catalog
-create_ip -name pfb_fir_2048ch_core -vendor User_Company -library SysGen -version 1.0 -module_name pfb_fir_2048ch_core_ip
