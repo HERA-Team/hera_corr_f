40846c296c593a580dd9adf80f570c971eb87cee
hera <hera@hera-snap-head.karoo.kat.ac.za>
2018-09-07 14:20:11 +0000

    Add helper function for configuring loggers

---
+++ control_software/src/helpers.py
@@ -0,0 +1,65 @@
+import numpy as np
+import logging
+import logging.handlers
+import sys
+import redis
+import json
+import socket
+import struct
+
+'''
+A Redis-based log handler from:
+http://charlesleifer.com/blog/using-redis-pub-sub-and-irc-for-error-logging-with-python/
+'''
+class RedisHandler(logging.Handler):
+    def __init__(self, channel, conn, *args, **kwargs):
+        logging.Handler.__init__(self, *args, **kwargs)
+        self.channel = channel
+        self.redis_conn = conn
+
+    def emit(self, record):
+        attributes = [
+            'name', 'msg', 'levelname', 'levelno', 'pathname', 'filename',
+            'module', 'lineno', 'funcName', 'created', 'msecs', 'relativeCreated',
+            'thread', 'threadName', 'process', 'processName',
+        ]
+        record_dict = dict((attr, getattr(record, attr)) for attr in attributes)
+        record_dict['formatted'] = self.format(record)
+        try:
+            self.redis_conn.publish(self.channel, json.dumps(record_dict))
+        except UnicodeDecodeError:
+            self.redis_conn.publish(self.channel, 'UnicodeDecodeError on emit!')
+        except redis.RedisError:
+            pass
+    
+def add_default_log_handlers(logger, redishostname='redishost', fglevel=logging.INFO, bglevel=logging.INFO):
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+    formatter = logging.Formatter('%(asctime)s - %(name)20s - %(levelname)s - %(message)s')
+
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    stream_handler.setLevel(fglevel)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+    syslog_handler.setLevel(bglevel)
+    syslog_handler.setFormatter(formatter)
+    logger.addHandler(syslog_handler)
+
+    redis_host = redis.Redis(redishostname, socket_timeout=1)
+    try:
+        redis_host.ping()
+    except redis.ConnectionError:
+        logger.warn("Couldn't connect to redis server at %s"%redishostname)
+        return logger
+
+    redis_handler = RedisHandler('log-channel', redis_host)
+    redis_handler.setLevel(bglevel)
+    redis_handler.setFormatter(formatter)
+    logger.addHandler(redis_handler)
+
+    logger.info("Logger %s created..."%logger.name)
+
+    return logger
