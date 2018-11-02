b55be76db0d100c737c455a00cdb70254e7b301f
Deepthi Gorthi <deepthigorthi@berkeley.edu>
2018-11-01 19:50:58 -0700

    Include poco in monitoring

--- control_software/scripts/hera_snap_redis_monitor.py
+++ control_software/scripts/hera_snap_redis_monitor.py
@@ -41,6 +41,42 @@ def get_fem_stats():
     """
     pass
 
+def get_poco_output(feng,redishost):
+    """
+    Get pocket correlator output. Antennas and integration time 
+    polled from redis database.
+    Params:  feng: SnapFengine object that maps to a SNAP board
+             redishost: Redis database to upload the data to.
+    Returns: Dict: {'data':shape(fqs, pols), 
+                    'times':list of unix times}
+    """
+    int_time = int(redishost.hget('poco', 'integration_time'))
+    acc_len = int_time * 500e6/ (8192 * feng.corr.spec_per_acc * 2)
+    
+    if (acc_len != feng.corr.get_acc_len()):
+        feng.corr.set_acc_len(acc_len)
+  
+    antenna_pairs = ([[0,0],[0,1],[0,2],[1,1],[1,2],[2,2]])
+    pair = (int(redishost.hget('poco','ant1')), int(redishost.hget('poco','ant2')))
+    idx = antenna_pairs.index(pair)
+    ant1, ant2 = antenna_pairs[(idx+1)%len(antenna_pairs)]
+    redishost.hset('poco','ant1',ant1)
+    redishost.hset('poco','ant2',ant2)    
+
+    ant1 *= 2; ant2 *= 2
+    xcorr = []; times = []
+    for i in range(4):
+        xcorr.append(feng.corr.get_new_corr(ant1+i%2, (ant2+(i//2+i%2)%2)))
+        times.append(time.time())
+    xcorr = np.asarray(xcorr); times = np.asarray(times)
+    redishost.hset('poco', 'corr',  xcorr.tostring())
+    redishost.hset('poco', 'times', times.tostring())
+
+    # to unpack from string: c = struct.unpack('<8192d'); xcorr = (c[::2] + 1j*c[1::2]).reshape(4,1024)
+   
+    return {'data':xcorr, 'times':times}
+    
+    
 
 if __name__ == "__main__":
     
@@ -50,6 +86,8 @@ if __name__ == "__main__":
                         help ='Host servicing redis requests')
     parser.add_argument('-d', dest='delay', type=float, default=10.0,
                         help ='Seconds between polling loops')
+    parser.add_argument('-c', dest='poco',action='store_true',default=False,
+                        help='Upload pocket correlator output to redis')
 
     args = parser.parse_args()
 
@@ -86,6 +124,9 @@ if __name__ == "__main__":
                     hist_bins, hist_vals = feng.input.get_histogram(chan, sum_cores=True)
                     corr.r.hset(status_key, 'histogram', json.dumps([hist_bins.tolist(), hist_vals.tolist()]))
                     corr.r.hset(status_key, 'timestamp', datetime.datetime.now().isoformat())
+
+                    if args.poco: get_poco_output(feng,corr.r)
+                        
                 except:
                     logger.error('Failed to get stats for ant %s pol %s (feng %s, chan %s)' % (ant, pol, feng, chan))
 
@@ -94,7 +135,7 @@ if __name__ == "__main__":
                 corr.r.hmset("status:snap:%s" % feng.host, get_fpga_stats(feng))
             except:
                 logger.error('Failed to get stats from SNAP %s' % feng.host)
-                
+        
         # If executing the loop hasn't already taken longer than the loop delay time, add extra wait.
         extra_delay = args.delay - (time.time() - tick)
         if extra_delay > 0:
