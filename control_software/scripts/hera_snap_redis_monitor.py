08b84d8b9bd41a2f3b1d01c27cf3a886cb4094b9
hera <hera@hera-snap-head.karoo.kat.ac.za>
2019-03-11 21:38:10 +0000

    call isoformat() properly

--- control_software/scripts/hera_snap_redis_monitor.py
+++ control_software/scripts/hera_snap_redis_monitor.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
         corr.compute_hookup()
 
         # load this module's version into redis
-        corr.r.hmset('version:%s' % __package__, {'version':__version__, 'timestamp':datetime.datetime.now().isoformat})
+        corr.r.hmset('version:%s' % __package__, {'version':__version__, 'timestamp':datetime.datetime.now().isoformat()})
 
         # Get antenna stats
         input_stats = corr.do_for_all_f("get_stats", block="input", kwargs={"sum_cores" : True})
