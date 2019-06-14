608557fa696962498c4cb1a6b8d1acedb197f7c2
hera <hera@hera-snap-head.karoo.kat.ac.za>
2019-06-14 23:23:41 +0000

    Add HERA CM log handler

--- control_software/src/helpers.py
+++ control_software/src/helpers.py
@@ -1,4 +1,3 @@
-import numpy as np
 import logging
 import logging.handlers
 import sys
@@ -35,8 +34,29 @@ class RedisHandler(logging.Handler):
             self.redis_conn.publish(self.channel, 'UnicodeDecodeError on emit!')
         except redis.RedisError:
             pass
+
+class HeraMCHandler(logging.Handler):
+    def __init__(self, subsystem, *args, **kwargs):
+        from hera_mc import mc
+        from astropy.time import Time
+        self.Time = Time
+        logging.Handler.__init__(self, *args, **kwargs)
+        self.subsystem = subsystem
+        parser = mc.get_mc_argument_parser()
+        args = parser.parse_args()
+        db = mc.connect_to_mc_db(args)
+        self.session = db.sessionmaker()
+
+    def emit(self, record):
+        # Re-code level because HeraMC logs 1 as most severe, and python logging calls critical:50, debug:10
+        severity = max(1, 100 / record.levelno)
+        message = self.format(record)
+        logtime = self.Time(time.time(), format="unix")
+        self.session.add_subsystem_error(logtime, self.subsystem, severity, message)
+            
+            
     
-def add_default_log_handlers(logger, redishostname='redishost', fglevel=logging.INFO, bglevel=logging.INFO):
+def add_default_log_handlers(logger, redishostname='redishost', fglevel=logging.INFO, bglevel=logging.INFO, include_mc=True, mc_level=logging.WARNING):
     logger.setLevel(logging.DEBUG)
     logger.propagate = False
 
@@ -51,6 +71,15 @@ def add_default_log_handlers(logger, redishostname='redishost', fglevel=logging.
     syslog_handler.setLevel(bglevel)
     syslog_handler.setFormatter(formatter)
     logger.addHandler(syslog_handler)
+
+    if include_mc:
+        try:
+            mc_handler = HeraMCHandler("correlator")
+            mc_handler.setLevel(mc_level)
+            mc_handler.setFormatter(formatter)
+            logger.addHandler(mc_handler)
+        except:
+            logger.warn("Failed to add HERA MC log handler")
 
     redis_host = redis.Redis(redishostname, socket_timeout=1)
     try:
