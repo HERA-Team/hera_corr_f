134a8a0fd0144a2367c759e8278a1c267289fc1e
David DeBoer <ddeboer@berkeley.edu>
2020-02-26 19:42:48 -0800

    updated hera_snap_ping monitor

--- control_software/scripts/hera_snap_ping.py
+++ control_software/scripts/hera_snap_ping.py
@@ -1,8 +1,29 @@
-from hera_corr_f.hera_corr import HeraCorrelator
-import os
-hera = HeraCorrelator()
-hosts = [s.host for s in hera.fengs]
-for h in hosts:
-    print(h)
-    os.system("ping -c 1 "+h)
-    print("\n")
+"""SNAP ping monitor."""
+import subprocess
+import redis
+import json
+from datetime import datetime
+from os import path
+
+
+r = redis.Redis('redishost')
+snaps_cm = list(json.loads(r.hget('corr:map', 'all_snap_inputs')).keys())
+snap_to_host = json.loads(r.hget('corr:map', 'snap_host'))
+
+now = datetime.strftime(datetime.now(), '%Y/%m/%d %H:%M')
+path_to_write = path.join(path.expanduser('~'), 'logs')
+
+for snap in snaps_cm:
+    try:
+        snap_ping = 'ping -c 1 {}'.format(snap_to_host[snap])
+    except KeyError:
+        continue
+    try:
+        stats = subprocess.check_output(snap_ping, shell=True).splitlines()
+    except subprocess.CalledProcessError:
+        continue
+
+    packets = 1.0 - float(stats[4].split(',')[2].strip().split()[0].strip('%')) / 100.0
+    fn = path.join(path_to_write, '{}.txt'.format(snap_to_host[snap]))
+    with open(fn, 'a') as fp:
+        print('{}\t{}\t{:.2f}'.format(now, snap, packets), file=fp)
