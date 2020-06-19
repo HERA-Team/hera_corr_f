3d4cc79874d61fcecd9481bb59f40f324104b111
Jonathon Kocz <1220894+jkocz@users.noreply.github.com>
2020-06-19 14:43:02 -0700

    control_software py3 updates

--- control_software/src/__init__.py
+++ control_software/src/__init__.py
@@ -1,5 +1,5 @@
-from __version__ import __version__
+from .__version__ import __version__
 
 #from roach_fengine import RoachFengine
-from snap_fengine import SnapFengine
-from hera_corr import HeraCorrelator
+from .snap_fengine import SnapFengine
+from .hera_corr import HeraCorrelator
