5ce5e73abcc82a02feffa3a2bdf1d658afe5b80b
Aaron Parsons <aparsons@berkeley.edu>
2021-10-16 18:01:07 +0000

    Added logging to start script

--- control_software/scripts/hera_start_feng.sh
+++ control_software/scripts/hera_start_feng.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-echo hera_start_feng.sh: Initializing F-Engines
-hera_snap_feng_init.py -p -i -d
+LOGFILE=snap_start.log
+ERRFILE=snap_start.err
 
-echo
-echo
+echo hera_start_feng.sh: Initializing F-Engines
+hera_snap_feng_init.py -p -i -d > $LOGFILE 2> $ERRFILE
+
+echo >> $LOGFILE
+echo >> $LOGFILE
+echo >> $ERRFILE
+echo >> $ERRFILE
 echo hera_start_feng.sh: Synchronizing and Starting TX
-hera_snap_feng_init.py -s -e
+hera_snap_feng_init.py -s -e >> $LOGFILE 2>> $ERRFILE
