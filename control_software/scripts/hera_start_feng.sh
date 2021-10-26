cf8441a4a6773d49f0eaf17eb0a343a36b0cdc94
Aaron Parsons <aparsons@berkeley.edu>
2021-10-26 23:02:38 +0000

    Added a hera_start_feng.sh script to automate startup and included sh files in the startup install

--- control_software/scripts/hera_start_feng.sh
+++ control_software/scripts/hera_start_feng.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
-LOGFILE=snap_start.log
-ERRFILE=snap_start.err
+LOGFILE=~/snap_start.log
+ERRFILE=~/snap_start.err
 
-echo hera_start_feng.sh: Initializing F-Engines
-hera_snap_feng_init.py -p -i -d > $LOGFILE 2> $ERRFILE
+echo hera_start_feng.sh: Initializing F-Engines > $LOGFILE
+hera_snap_feng_init.py -p -i -d >> $LOGFILE 2> $ERRFILE
 
 echo >> $LOGFILE
 echo >> $LOGFILE
