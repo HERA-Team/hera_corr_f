e85e3ca0f2dc242dd42cdaaea14f92cb9e3f7833
Aaron Parsons <aparsons@berkeley.edu>
2021-11-02 22:10:12 +0000

    Fixed logging on hera_feng_start.sh to append

--- control_software/scripts/hera_feng_start.sh
+++ control_software/scripts/hera_feng_start.sh
@@ -7,9 +7,9 @@ set -e
 
 echo hera_start_feng.sh: Initializing F-Engines > $LOGFILE
 date >> $LOGFILE
-hera_snap_feng_init.py -p -i -d &> $LOGFILE
+hera_snap_feng_init.py -p -i -d &>> $LOGFILE
 
 echo >> $LOGFILE
 echo >> $LOGFILE
 echo hera_start_feng.sh: Synchronizing and Starting TX
-hera_snap_feng_init.py -s -e &> $LOGFILE
+hera_snap_feng_init.py -s -e &>> $LOGFILE
