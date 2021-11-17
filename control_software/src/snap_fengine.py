10a7b8395d8b5d9b7cdccb7aa55d38095a529a21
Jonathon Kocz <1220894+jkocz@users.noreply.github.com>
2021-11-17 09:58:09 -0800

    add back noise block init to snap_fengine.py

--- control_software/src/snap_fengine.py
+++ control_software/src/snap_fengine.py
@@ -40,7 +40,7 @@ class SnapFengine(object):
         self.synth = Synth(self.fpga, 'lmx_ctrl')
         self.adc = Adc(self.fpga) # not a subclass of Block
         self.sync = Sync(self.fpga, 'sync')
-        #self.noise = NoiseGen(self.fpga, 'noise', nstreams=3)
+        self.noise = NoiseGen(self.fpga, 'noise', nstreams=3)
         self.input = Input(self.fpga, 'input', nstreams=12)
         self.delay = Delay(self.fpga, 'delay', nstreams=6)
         self.pfb = Pfb(self.fpga, 'pfb')
@@ -64,7 +64,7 @@ class SnapFengine(object):
             self.synth,
             self.adc,
             self.sync,
-            #self.noise, # temporarily removed
+            self.noise,
             self.input,
             self.delay,
             self.pfb,
