9b6d01478bcef705639cc6acaf3fe89808d95efa
hera <hera@hera-snap-head.karoo.kat.ac.za>
2022-04-02 16:07:11 +0000

    changing maintenance signal path input from load to sky

--- control_software/scripts/hera_feng_start_maintenance.sh
+++ control_software/scripts/hera_feng_start_maintenance.sh
@@ -8,10 +8,11 @@ set -e
 echo hera_feng_start.sh: Initializing F-Engines > $LOGFILE
 date >> $LOGFILE
 hera_snap_feng_init.py -p -i -d &>> $LOGFILE
-echo hera_fem_control.py: Setting FEMs to LOAD >> $LOGFILE
-hera_fem_control.py --state=load &>> $LOGFILE
-echo hera_snap_enable_digital_noise.py --source=noise --seed=same >> $LOGFILE
-hera_snap_enable_digital_noise.py --source=noise --seed=same &>> $LOGFILE
+#fem_control options are noise,load,antenna
+echo hera_fem_control.py: Setting FEMs to ANTENNA >> $LOGFILE
+hera_fem_control.py --state=antenna &>> $LOGFILE
+#echo hera_snap_enable_digital_noise.py --source=adc --seed=diff >> $LOGFILE
+#hera_snap_enable_digital_noise.py --source=adc --seed=diff &>> $LOGFILE
 echo >> $LOGFILE
 echo >> $LOGFILE
 echo hera_feng_start.sh: Synchronizing and Starting TX >> $LOGFILE
