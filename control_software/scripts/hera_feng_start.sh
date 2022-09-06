372a9300bdb9aa62445d514378dfd7ac6c2df963
hera <hera@hera-snap-head.karoo.kat.ac.za>
2022-09-06 17:23:54 +0000

    Made number of enabled SNAP ethernets a parameter for hera_feng_start with default 44 (not 46)

--- control_software/scripts/hera_feng_start.sh
+++ control_software/scripts/hera_feng_start.sh
@@ -5,9 +5,34 @@ LOGFILE=~/snap_start.log
 source ~/.bashrc
 set -e
 
-echo hera_feng_start.sh: Initializing F-Engines > $LOGFILE
+Help()
+{
+    echo "Options:"
+    echo "m     Maximum SNAP ethernets to enable. Default 44"
+    echo
+}
+
+# Default values
+maxeth="44"
+
+# Process input arguments
+while getopts "m:" option; do
+    case $option in
+        h) # display Help
+            Help
+            exit;;
+        m) # maxeth
+            maxeth="$OPTARG"
+            ;;
+        \?) # Invalid option
+            Help
+            exit;;
+    esac
+done
+
+echo hera_feng_start.sh: Initializing up to ${maxeth} F-Engines > $LOGFILE
 date >> $LOGFILE
-hera_snap_feng_init.py -p -i -d -s -e --max_eth_enabled=46 &>> $LOGFILE
+hera_snap_feng_init.py -p -i -d -s -e --max_eth_enabled=${maxeth} &>> $LOGFILE
 
 echo >> $LOGFILE
 echo >> $LOGFILE
